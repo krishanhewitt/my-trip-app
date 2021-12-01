1e7791ed742b15288dfc3f3ea847961c3820f937
Krishan <krishanhewitt@hotmail.com>
2021-12-01 09:19:06 -0800

    Initial environment setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, {createGlobalStyle} from "styled-components";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyleSheet.Wrapper>
+      <CSSReset />
+    </StyleSheet.Wrapper>
   );
 }
+
+const Styles = {
+  Wrapper: styled.main`
+    display: flex;
+    background-color: #eeeeee;
+    height: 100vh;
+  `,
+};
+
+const CSSReset = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+  }
+  
+  html {
+    font-size: 62.5%;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-size: 1.4rem;
+    font-family: sans-serif;
+  }
+`;
 
 export default App;
