a1a914ca2e385148e533b2b79ba8851bdee3386b
Krishan <krishanhewitt@hotmail.com>
2022-01-16 10:03:39 -0800

    Converted ShowTripDetail from class to functional component

--- src/components/trips_page/TripCard.js
+++ src/components/trips_page/TripCard.js
@@ -9,7 +9,7 @@ const TripCard = (props) => {
     return(
         <div className="card-container w-50 m-4 text-center rounded-lg shadow-xl">
             <p className='font-medium'>
-                <Link to={`/show-trip/${trip._id}`}>{ trip.name }</Link>
+                <Link to={`/show-trip/${trip._id}`} state={{tripID: trip._id}}>{ trip.name }</Link>
             </p>
             <img src="https://images.unsplash.com/photo-1538681105587-85640961bf8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" alt="" />
             <p>{trip.origin} &#8594; {trip.destination}</p>
