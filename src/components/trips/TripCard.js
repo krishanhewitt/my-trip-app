8a12ad5af70051f07cfdc0e6e56b637fad8b3b0d
Krishan <krishanhewitt@hotmail.com>
2022-02-24 09:11:03 -0800

    TripCard: added hover effect, updated placeholder image, and made entire card clickable

--- src/components/trips/TripCard.js
+++ src/components/trips/TripCard.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import mtn from "../../devImages/mountain.jpg";
 
 export default function TripCard(props) {
-    const trip  = props.trip;
-    const parsedStartDate = new Date(trip.startDate).toISOString().slice(0, 10);
-    const parsedEndDate = new Date(trip.endDate).toISOString().slice(0, 10);
+  const trip = props.trip;
+  const parsedStartDate = new Date(trip.startDate).toLocaleDateString("en-CA");
+  const parsedEndDate = new Date(trip.endDate).toLocaleDateString("en-CA");
 
-    return(
-        <div className="card-container w-25 m-4 text-center rounded-lg shadow-xl">
-            <p className='font-medium'>
-                <Link to={`/show-trip/${trip._id}`} state={{tripID: trip._id}}>{ trip.name }</Link>
+  return (
+    <Link to={`/show-trip/${trip._id}`} state={{ tripID: trip._id }}>
+      <div className="w-64 m-4 rounded-lg shadow-[0_17px_10px_-10px_rgba(0,0,0,0.4)] transition ease-in-out duration-300 hover:-translate-y-4 hover:shadow-[0_37px_20px_-15px_rgba(0,0,0,0.2)]">
+        <img className="h-48 object-cover" src={mtn} alt="" />
+        <div className="px-6 py-4">
+          <p className="font-medium text-xl mb-2">{trip.name}</p>
+          <div className="text-gray-700 text-base">
+            <p>
+              {trip.origin} &#8594; {trip.destination}
             </p>
-            <img src="https://images.unsplash.com/photo-1538681105587-85640961bf8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" alt="" />
-            <p>{trip.origin} &#8594; {trip.destination}</p>
-            <p>{parsedStartDate} &#8594; {parsedEndDate}</p>
+            <p>
+              {parsedStartDate} &#8594; {parsedEndDate}
+            </p>
+          </div>
         </div>
-    )
-};
+      </div>
+    </Link>
+  );
+}
